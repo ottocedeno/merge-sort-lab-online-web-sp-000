8ec07b28e499b11b96adb1f2bc7ba05886c679f9
Otto Cedeno <otto@creativespaceclub.com>
2020-12-20 09:27:14 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+function findMinAndRemoveSorted(array) {
+  let min = array[0];
+  let minIndex = 0;
 
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
+}
+
+function merge(firstSubarray, secondSubarray) {
+  let sorted = [];
+  let currentMin;
+
+  while (firstSubarray.length != 0 && secondSubarray.length != 0) {
+    if (firstSubarray[0] < secondSubarray[0]) {
+      currentMin = firstSubarray.shift();
+    } else {
+      currentMin = secondSubarray.shift();
+    }
+    sorted.push(currentMin);
+  }
+  return sorted.concat(firstSubarray).concat(secondSubarray);
+}
+
+function mergeSort(array) {
+  let midpoint = array.length / 2;
+  let firstHalf = array.slice(0, midpoint);
+  let secondHalf = array.slice(midpoint, array.length);
+
+  if (array.length < 2) {
+    return array;
+  } else {
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+  }
+}
+
+// console.log(mergeSort([12, 10, 9, 14, 1, 3, 5, 11, 6, 15, 16, 13, 2, 4, 8, 7]));
